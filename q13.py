98eb6f64a8aa55f5acd44e714396a5738b028b5b
komal-Chaudhary-09 <komalchoudhary260408@gmail.com>
2026-07-16 18:47:16 +0530

    practice

---
+++ q13.py
@@ -0,0 +1,20 @@
+# Find all repeating elements in an array
+from collections import Counter
+
+def sample(arr):
+    count = Counter(arr)
+    result = []
+    for element in arr:
+        if count[element] > 1 and element not in result:
+            result.append(element)
+    return result
+
+def main():
+    arr = list(map(int, input().split()))
+    print(sample(arr))
+    
+if __name__ == "__main__":
+    main()
+   
+                
+            
