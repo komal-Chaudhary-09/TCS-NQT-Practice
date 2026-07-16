6b29bc81da12b4eebb3d1b0f36390dcaaa270063
komal-Chaudhary-09 <komalchoudhary260408@gmail.com>
2026-07-16 18:20:57 +0530

    practice

---
+++ q11.py
@@ -0,0 +1,23 @@
+class Solution:
+    def sample(self, arr):
+        seen = {}
+        result = []
+        
+        for val in arr:
+            if val not in seen:
+                result.append(val)
+                seen[val] = True
+        return result
+    
+def main():
+    arr = list(map(int, input().split()))
+    obj = Solution()
+    print(obj.sample(arr))
+if __name__ == "__main__":
+    main()
+            
+                
+            
+        
+    
+        
