730cf1a20dfa18126f57c9d705e6479be8b7b829
komal-Chaudhary-09 <komalchoudhary260408@gmail.com>
2026-07-16 17:23:05 +0530

    practice

---
+++ q7.py
@@ -0,0 +1,27 @@
+class Solution:
+    def reverse(self, arr, start, end):
+        while start < end:
+            arr[start], arr[end] = arr[end], arr[start]
+            start += 1
+            end -= 1
+    
+    def leftRotate (self, arr, k):
+        n = len(arr)
+        k = k % n
+        
+        self.reverse(arr, 0 , k-1)
+        self.reverse(arr, k, n-1)
+        self.reverse(arr, 0, n-1)
+        
+        return arr
+    
+def main():
+    arr = list(map(int, input().split()))
+    k = int(input())
+    obj = Solution()
+    print(obj.leftRotate(arr,k))
+if __name__ == "__main__":
+    main()
+        
+        
+        
