17bf32b90bdb93f3ebb4a565017412d679276462
komal-Chaudhary-09 <komalchoudhary260408@gmail.com>
2026-07-17 16:45:51 +0530

    practice

--- q15.py
+++ q15.py
@@ -1,17 +1,32 @@
+# Maximum Product Subarray in an Array
 class Solution:
     def sample(self, arr):
-        seen = {}
-        result = []
-        
-        for a,b in arr:
-            if (b,a) in seen:
-                result.append((a,b))
-            else:
-                seen[(a,b)] = True
-        return result
-    
+        n = len(arr)
+
+        prefix = 1
+        suffix = 1
+        ans = arr[0]
+
+        for i in range(n):
+
+            if prefix == 0:
+                prefix = 1
+
+            if suffix == 0:
+                suffix = 1
+
+            prefix *= arr[i]
+            suffix *= arr[n - 1 - i]
+
+            ans = max(ans, prefix, suffix)
+
+        return ans
+
+
 def main():
     arr = list(map(int, input().split()))
-    
-        
-        
+    obj = Solution()
+    print(obj.sample(arr))
+
+if __name__ == "__main__":
+    main()
