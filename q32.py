d7f1749f06149e9f4d9ea9ca314a46faa19e561b
komal-Chaudhary-09 <komalchoudhary260408@gmail.com>
2026-07-18 09:34:08 +0530

    practice

---
+++ q32.py
@@ -0,0 +1,12 @@
+# Reverse a String
+class Solution:
+    def sample(self, s):
+        return s[::-1]
+def main():
+    s = input()
+    obj = Solution()
+    print(obj.sample(s))
+    
+if __name__ == "__main__":
+    main()
+        
